2e31cfec93d3a3bea5ba629ebd71a25b8dc7c26b
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2022-09-15 16:43:59 +0500

    refactored usings

--- src/ZenExtensions.Slack.Models/Attachment.cs
+++ src/ZenExtensions.Slack.Models/Attachment.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace ZenExtensions.Slack.Models
 {
