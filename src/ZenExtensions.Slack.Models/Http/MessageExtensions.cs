f92512e7d7bc62c80e1f44260f3cfba665c9d97e
WajahatAliAbid <WajahatAliAbid@users.noreply.github.com>
2022-09-17 15:33:04 +0500

    added additional artifacts

--- src/ZenExtensions.Slack.Models/Http/MessageExtensions.cs
+++ src/ZenExtensions.Slack.Models/Http/MessageExtensions.cs
@@ -12,15 +12,15 @@ namespace ZenExtensions.Slack.Models.Http
         /// Sends a <see cref="Message"/> to the endpoint specified
         /// </summary>
         /// <param name="message">Message you want to send </param>
-        /// <param name="responseUrl">Url you want to send message to</param>
+        /// <param name="endpoint">Url you want to send message to</param>
         /// <returns><see cref="Task"/> indicating completion of sent process</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="message"/> or <paramref name="responseUrl"/> is null</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="message"/> or <paramref name="endpoint"/> is null</exception>
         /// <exception cref="System.Net.Http.HttpRequestException"></exception>
-        public static async Task SendAsync(this Message message, string responseUrl)
+        public static async Task SendAsync(this Message message, string endpoint)
         {
             ArgumentNullException.ThrowIfNull(message, nameof(message));
-            ArgumentNullException.ThrowIfNull(responseUrl, nameof(responseUrl));
-            var response = await _client.PostAsJsonAsync(responseUrl, message);
+            ArgumentNullException.ThrowIfNull(endpoint, nameof(endpoint));
+            var response = await _client.PostAsJsonAsync(endpoint, message);
             response.EnsureSuccessStatusCode();
         }
     }
